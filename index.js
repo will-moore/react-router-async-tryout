adda7033ee37585a4e5dd5a6341358ab0852b095
William Moore <w.moore@dundee.ac.uk>
2016-09-21 22:43:52 +0100

    more cleanup and docs

--- index.js
+++ index.js
@@ -2,32 +2,20 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Link } from 'react-router'
 import { Router, Route, hashHistory, IndexRoute } from 'react-router'
-// import App from './modules/App'
-// import About from './modules/About'
-// import Repos from './modules/Repos'
-// import Repo from './modules/Repo'
-// import Home from './modules/Home'
 
 
-const Home = () => (
-	<div>Pick a user from links above to load data from github</div>
-)
-
-const NavLink = React.createClass({
-  render() {
-    return <Link {...this.props} activeClassName="active"/>
-  }
-})
-
+// Top level component, loaded on '/' and always visible.
+// props.children contains the rest of the App, as nested
+// is child Route components below.
 const App = React.createClass({
   render() {
     return (
       <div>
         <h1>React Router Tutorial</h1>
         <ul role="nav">
-          <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
-          <li><NavLink to="/users/will-moore">Will</NavLink></li>
-          <li><NavLink to="/users/bob">Bob</NavLink></li>
+          <li><Link to="/" onlyActiveOnIndex>Home</Link></li>
+          <li><Link to="/users/will-moore">Will</Link></li>
+          <li><Link to="/users/jburel">Jean Marie</Link></li>
         </ul>
         {this.props.children}
       </div>
@@ -35,6 +23,15 @@ const App = React.createClass({
   }
 })
 
+
+// Placeholder, shown as child of App at '/'
+const Home = () => (
+	<div>Pick a user from links above to load data from github</div>
+)
+
+
+// Child of App, passed :userName in props.params
+// from the url /users/:userName
 const User = React.createClass({
   render() {
     return (
@@ -46,6 +43,8 @@ const User = React.createClass({
 })
 
 
+// The main render method where we set up Routes
+// and load into #app element.
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
